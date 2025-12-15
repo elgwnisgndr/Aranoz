53418aab37f10519f4b54e2582dd1966cb5e73ed
madprogrammerelgwn <237120820+elgwnisgndr@users.noreply.github.com>
2025-12-15 07:19:06 +0400

    Add utility functions as comments in main.js
    Add commented-out functions for various tasks including summing odd numbers, finding min and max values, calculating total price, and filtering by category.

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+// const reqemler = [1, 2, 3, 4, 5];
+
+// function tekEdedlerinCemi(arr) {
+//   let cem = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 !== 0) {
+//       cem += arr[i];
+//     }
+//   }
+
+//   return cem;
+// }
+
+// console.log(tekEdedlerinCemi(reqemler)); /task 1
+
+// const ededler = [5, 3, 9, 1, 4];
+
+// function minMaxTap(arr) {
+//   let min = arr[0];
+//   let max = arr[0];
+
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//       min = arr[i];
+//     }
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+
+//   return { min, max };
+// }
+
+// console.log(minMaxTap(ededler));
+//task 2
+
+// const umumiQiymet = arr =>
+//   arr.reduce((cem, product) => cem + product.price, 0);
+
+// console.log(umumiQiymet(products)); //task 3
+// function categorySec(arr, category) {
+//   let netice = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].category === category) {
+//       netice.push(arr[i]);
+//     }
+//   }
+
+//   return netice;
+// }
+// console.log(categorySec(products, "electronics")); //task 4
